786a1def4f048411c87e0bf190d2ee455c049762
brunoressiguierbarbosa <bruno.ressiguier.barbosa@escola.pr.gov.br>
2024-06-20 08:15:02 -0300

    Add files via upload

---
+++ 2Boperacaoaritmetica/script.js
@@ -0,0 +1,39 @@
+// var a = 78;
+// var b = 15;
+// var total = a + b;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total = a - b;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total = a - b;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total = a * b;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total = a / b;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total = a % b;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total = a ++ b;
+// console.log(total);
+
+// var a = 78;
+// var b = 15;
+// var total = a -- b;
+// console.log(total);
